a92acddb80c7f779c32b07e3695904f73a9117de
Kusmayadi <kusmayadi@cobacobi.com>
2012-11-18 00:19:51 +0700

    Removed config file

--- config/infusionsoft.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array (
-    
-    'api_service_url'   => '',
-    'api_passphrase'    => '',
-    'encrypted_key'     => '',
-    
-);
