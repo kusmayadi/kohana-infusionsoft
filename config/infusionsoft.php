accc9d03eb5a5bde5e7e46761c2e4f8fcdcd3e59
Kusmayadi <kusmayadi@cobacobi.com>
2012-11-18 00:01:49 +0700

    Added infusionsoft config file

---
+++ config/infusionsoft.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array (
+    
+    'api_service_url'   => '',
+    'api_passphrase'    => '',
+    'encrypted_key'     => '',
+    
+);
